a95eae3d7a2f3c46692a1db180cf35f0e2cedb43
hhhhjjjjjjiiii <wnlwnl0522@gmail.com>
2021-11-18 16:25:48 +0900

    test2

--- jenkins.sh
+++ jenkins.sh
@@ -1,3 +1,4 @@
 mkdir /jenkins
-docker run -itd -p 60800:8080 -v /root/jenkins:/var/jenkins_home --name j1 jenkins/jenkins:lts
+chmod 777 /jenkins
+docker run -itd -p 60800:8080 -v /jenkins:/var/jenkins_home --name j1 jenkins/jenkins:lts
 docker exec j1 cat /var/jenkins_home/secrets/initialAdminPassword
